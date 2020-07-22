eb9972445b0956a193a5e803cbbefc242f10b5be
liz <micheal naita>
2020-07-22 16:37:27 +0300

    updated to database

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
 	return (
 		<div>
-			<div className="p f3 ">{`${name}, your current rank is..`}</div>
+			<div className="p f3 ">{`${name}, your current entry count is..`}</div>
 			<div className="p f1 ">{entries}</div>
 		</div>
 	);
